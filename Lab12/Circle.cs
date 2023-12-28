5e1054e6bd13d67c7e3605eb223d74c3ae7d9716
Артём Белоусов <belousov@psksk.ru>
2023-12-28 09:14:41 +0300

    Добавьте файлы проекта.

---
+++ Lab12/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab12
+{
+    static class Circle
+    {
+        static public double GetLength(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+        static public double GetArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        static public bool IsPointInsideCircle(double x, double y, double x0, double y0, double radius)
+        {
+            double distance = Math.Sqrt(Math.Pow(x - x0, 2) + Math.Pow(y - y0, 2));
+
+            return distance < radius;
+        }
+    }
+}
---
+++ Lab12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+
+
+            double length = Circle.GetLength(radius);
+            double area = Circle.GetArea(radius);
+
+            Console.WriteLine($"Длина окружности: {length}");
+            Console.WriteLine($"Площадь круга: {area}");
+
+            Console.WriteLine("Введите координаты точки (x, y) для проверки принадлежности кругу:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            bool isInside = Circle.IsPointInsideCircle(x, y, 0, 0, radius);
+
+            Console.WriteLine($"Точка ({x}, {y}) принадлежит кругу: {isInside}");
+            Console.ReadKey();
+        }
+    }
+}
